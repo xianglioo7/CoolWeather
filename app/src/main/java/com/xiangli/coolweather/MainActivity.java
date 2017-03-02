749e269e9af3231dd3eca7f6601914840307e7d5
xiangli <xianglioo7@163.com>
2017-03-02 23:34:05 +0800

    t sfajlslfaf" dfklq

--- app/src/main/java/com/xiangli/coolweather/MainActivity.java
+++ app/src/main/java/com/xiangli/coolweather/MainActivity.java
@@ -2,16 +2,7 @@ package com.xiangli.coolweather;
 
 import android.os.Bundle;
 
-import com.google.gson.Gson;
 
-import butterknife.ButterKnife;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setcontentview(R.layout.activity_main);
         int a =2;
         String.valueOf(a);
+        String s = "2123";
 
 
 
